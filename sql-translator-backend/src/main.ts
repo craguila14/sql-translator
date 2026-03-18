648a86931d5dacc9ff860e02615bc65944559239
craguila14 <craguilaasenjo@gmail.com>
2026-03-18 01:49:34 -0300

    url front added

--- sql-translator-backend/src/main.ts
+++ sql-translator-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: `${process.env.ORIGIN}`, 
+    origin: process.env.ORIGIN, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
